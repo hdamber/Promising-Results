5d0740721d0f3ccd969b57c58f826ea33c4d18a1
IB_Student <student@InnovateBhams-MacBook-Pro-4.local>
2020-02-13 14:05:05 -0600

    finished

--- scripts.js
+++ scripts.js
@@ -1,34 +1,61 @@
 //Promise.then() chaining
-slowMath.add(6, 2)
-    .then(num => {
-        console.log(num);
-        return slowMath.multiply(num, 2);
-    }).then(num => {
-        console.log(num);
-        return slowMath.divide(num, 4);
-    }).then(num => {
-        console.log(num);
-        return slowMath.subtract(num, 3);
-    }).then(num => {
-        console.log(num);
-        return slowMath.add(num, 98);
-    }).then(num => {
-        console.log(num);
-        return slowMath.divide(num, 2);
-    }).then(num => {
-        console.log(num);
-        return slowMath.multiply(num, 50);
-    }).then(num => {
-        console.log(num);
-        return slowMath.divide(num, 40);
-    }).then(num => {
-        console.log(num);
-        return slowMath.add(num, 32); 
-    }).then(num => {
-        console.log(`The final result is ${num}.`)
-    }).catch(err => {
-        console.log(err)
-    })
+// slowMath.add(2, 6)
+//     .then(num => {
+//         console.log(num);
+//         return slowMath.multiply(num, 2);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.divide(num, 4);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.subtract(num, 3);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.add(num, 98);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.remainder(num, 2);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.multiply(num, 50);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.remainder(num, 40);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.add(num, 32); 
+//     }).then(num => {
+//         console.log(`The final result is ${num}.`)
+//     }).catch(err => {
+//         console.log(err)
+//     })
 
 // async/await
+async function doMath() {
+    try {
+        let num = await slowMath.add(2, 6);
+        console.log(num);
+        num = await slowMath.multiply(num, 2);
+        console.log(num);
+        num = await slowMath.divide(num, 4);
+        console.log(num);
+        num = await slowMath.subtract(num, 3);
+        console.log(num);
+        num = await slowMath.add(num, 98);
+        console.log(num);
+        num = await slowMath.remainder(num, 2);
+        console.log(num);
+        num = await slowMath.multiply(num, 50);
+        console.log(num);
+        num = await slowMath.remainder(num, 40);
+        console.log(num);
+        num = await slowMath.add(num, 32);
+        console.log(num);
+            console.log(`The final result is ${num}.`)
 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+doMath();
