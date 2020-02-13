3aa11c9712b389a8d503015cc27cfc33c98f8a15
IB_Student <student@InnovateBhams-MacBook-Pro-4.local>
2020-02-13 11:20:57 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+//Promise.then() chaining
+slowMath.add(6, 2)
+    .then(num => {
+        console.log(num);
+        return slowMath.multiply(num, 2);
+    }).then(num => {
+        console.log(num);
+        return slowMath.divide(num, 4);
+    }).then(num => {
+        console.log(num);
+        return slowMath.subtract(num, 3);
+    }).then(num => {
+        console.log(num);
+        return slowMath.add(num, 98);
+    }).then(num => {
+        console.log(num);
+        return slowMath.divide(num, 2);
+    }).then(num => {
+        console.log(num);
+        return slowMath.multiply(num, 50);
+    }).then(num => {
+        console.log(num);
+        return slowMath.divide(num, 40);
+    }).then(num => {
+        console.log(num);
+        return slowMath.add(num, 32); 
+    }).then(num => {
+        console.log(`The final result is ${num}.`)
+    }).catch(err => {
+        console.log(err)
+    })
+
+// async/await
+
